f723f263442490fad7b6cc75936046d4e7da724f
TsukemonoGit <mine43try@outlook.com>
2025-05-03 21:02:09 +0900

    みためちょうせい

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import '../app.css';
 
 	let { children } = $props();
+	onMount(async () => {
+		const initNostrLogin = await import('nostr-login');
+		initNostrLogin.init({
+			/*options*/
+		});
+	});
 </script>
 
 {@render children()}
