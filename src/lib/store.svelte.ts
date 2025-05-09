20b45d575e0adfc22172cc34fcd931aad32f1a1d
TsukemonoGit <mine43try@outlook.com>
2025-05-09 21:29:24 +0900

    i18n

--- src/lib/store.svelte.ts
+++ src/lib/store.svelte.ts
@@ -3,6 +3,8 @@ import * as Nostr from 'nostr-typedef';
 export const articles = createCustomStore<Nostr.Event[]>([]);
 export const emojiList = createCustomStore<string[][]>([]);
 export const isDark = createCustomStore<boolean>(false);
+export const userLanguage = createCustomStore<string | null>(null);
+export const translations = createCustomStore<Record<string, string>>({});
 // 汎用的なカスタムストア作成関数
 function createCustomStore<T>(initialValue: T) {
 	let state: T = $state.raw(initialValue);
